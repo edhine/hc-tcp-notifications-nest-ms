a96c8f5054885fee588c0bdba525ca5e3131640a
Sergio Orellana <sergio.andres.orellana.roa@gmail.com>
2021-04-17 08:42:51 -0400

    init

---
+++ src/math/math.controller.ts
@@ -0,0 +1,11 @@
+
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+
+@Controller()
+export class MathController {
+  @MessagePattern({ cmd: 'sum' })
+  accumulate(data: number[]): number {
+    return (data || []).reduce((a, b) => a + b);
+  }
+}
